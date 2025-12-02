c26969ee8a99f913b6544bc5a2913ea07bc65816
RafaelRabel0 <rabelosouza03@gmail.com>
2025-12-02 16:59:34 -0300

    //Erro java: Index -1 out of bounds for length 10// - RESOLVIDO. O erro estava na utilização do Lombok, ao retirar o código funcionou. Implementação da visualização da tabela, adição, edição e exclusão de dados da tabela.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,10 @@
 module org.projeto_poo2_estoque {
     requires javafx.controls;
     requires javafx.fxml;
-    requires static lombok;
-    // requires jakarta.persistence;
-    // requires org.hibernate.orm.core;
-    // requires java.sql;
 
     opens org.projeto_poo2_estoque.model to javafx.base;
     opens org.projeto_poo2_estoque to javafx.fxml;
     exports org.projeto_poo2_estoque;
     exports org.projeto_poo2_estoque.controller;
     opens org.projeto_poo2_estoque.controller to javafx.fxml;
-}
 }
--- src/main/java/org/projeto_poo2_estoque/model/Produto.java
+++ src/main/java/org/projeto_poo2_estoque/model/Produto.java
@@ -1,20 +1,36 @@
 package org.projeto_poo2_estoque.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class Produto {
     private int id;
     private String nome;
     private double preco;
     private int quantidade;
 
+    public Produto() {
+    }
+
     public Produto(int id, String nome, double preco, int quantidade) {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
+    }
+
+    // Getters e Setters manuais
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
+
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+
+    @Override
+    public String toString() {
+        return "Produto{id=" + id + ", nome='" + nome + "', preco=" + preco + ", quantidade=" + quantidade + '}';
     }
 }
